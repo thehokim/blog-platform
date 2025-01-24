147ad3362bf385a9d70fc391f1e4958a2afb278e
thehokim <mr.hokimxoja@gmail.com>
2025-01-24 10:53:25 +0500

    Sherbek

--- backend/handlers/comments.go
+++ backend/handlers/comments.go
@@ -575,8 +575,8 @@ func GetReplyLikes(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]interface{}{
-		"likes":   likeCount,
-		"isLiked": isLiked,
+		"likes":        likeCount,
+		"isReplyLiked": isLiked,
 	})
 }
 
